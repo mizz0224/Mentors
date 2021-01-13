a93714c7d90bb49e771d07c248893f4f98496038
mizz0224 <wosud0224@naver.com>
2021-01-13 21:20:23 +0900

    update

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.deletion import CASCADE
-from mentors import models as models_Mentor
+
 
 # Create your models here.
 class User(AbstractUser):
@@ -18,11 +18,3 @@ class User(AbstractUser):
     )
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     is_mentors = models.BooleanField(default=False)
-    if is_mentors:
-        mentorfield = models.ForeignKey(
-            "Mentors", related_name="users", on_delete=CASCADE
-        )
-    else:
-        mentorfield = models.ForeignKey(
-            "Mentees", related_name="users", on_delete=CASCADE
-        )
