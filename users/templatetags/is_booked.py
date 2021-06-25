adfae755a551268cda4efa381a933cb0a89875a2
unknown <khawoo@naver.com>
2021-06-25 13:24:47 +0900

    end11

--- users/templatetags/is_booked.py
+++ users/templatetags/is_booked.py
@@ -6,13 +6,13 @@ from users import models as users_models
 register = template.Library()
 
 @register.simple_tag
-def is_booked(user, day):
+def is_booked(user, Mentor, day):
     if day.number == 0:
         return 
     try:
         # user = users_models.User.objects.get(pk=user_pk)
         date = datetime.datetime(year=day.year, month=day.month, day=day.number)
-        reservation = reservation_models.Reservation.objects.get(check_in=date, user=user)
+        reservation = reservation_models.Reservation.objects.get(check_in=date, user=user, mentor=Mentor)
         return True
     except reservation_models.Reservation.DoesNotExist:
         return False
